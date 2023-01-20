4e029a64bd78607325c0cdf06fb10ab4b48f41c7
Santiago Lopez <asantiago0034@gmail.com>
2023-01-20 02:50:42 -0500

    New footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,57 @@
-import icon from '../assets/Isotype.png';
+import logo from '../assets/Logotype.png';
+import insta from '../assets/icons/Instagram.png';
+import tiktok from '../assets/icons/TikTok.png';
+import twitter from '../assets/icons/Twitter.png';
+import face from '../assets/icons/Facebook.png';
+
 import '../styles/footer.css';
 
 const Footer = () => {
     return (
         <footer className='ab-footer'>
             <div className='ab-container ab-footer-container'>
-                <div className='ab-footer-col'>
-                    <h3>Contacto</h3>
-                    <a href="/" className='ab-footer-link'>hola@abunda.com.co</a>
-                    <h3>Contacto</h3>
-                    <a href="/" className='ab-footer-link'>hola@abunda.com.mx</a>
+                <div className='ab-footer-row ab-footer-logo'>
+                    <img src={logo} alt="Abunda Icon" />
                 </div>
-                <div className='ab-footer-col'>
-                    <h3>Redes Sociales</h3>
-                    <ul className='ab-footer-socials'>
-                        <li className='ab-footer-link'>Instagram</li>
-                        <li className='ab-footer-link'>TikTok</li>
-                        <li className='ab-footer-link'>Twitter</li>
-                        <li className='ab-footer-link'>Facebook</li>
-                    </ul>
+                <div className='ab-footer-row ab-footer-contact'>
+                    <div className="ab-footer-contact-cell">
+                        <h3>Contacto</h3>
+                        <a href="/" className='ab-footer-contact-link'>hola@abunda.com.co</a>
+                    </div>
+                    <div className="ab-footer-contact-cell">
+                        <h3>Contacto</h3>
+                        <a href="/" className='ab-footer-contact-link'>hola@abunda.com.mx</a>
+                    </div>
                 </div>
             </div>
-            <div className='ab-container'>
-                <img src={icon} alt="Abunda Icon" />
+            <div className='ab-container ab-footer-bottom-row'>
+                <div className='ab-footer-row'>
+                    <div className='ab-footer-privacy-info'>
+                        <a href="/" className='ab-footer-contact-link'>Politicas de Privacidad</a>
+                    </div>
+                    <ul className='ab-footer-socials'>
+                        <li>
+                            <a href="/" className='ab-footer-link'>
+                                <img src={insta} alt="Instagram" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/" className='ab-footer-link'>
+                                <img src={tiktok} alt="Tik Tok" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/" className='ab-footer-link'>
+                                <img src={twitter} alt="Twitter" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/" className='ab-footer-link'>
+                                <img src={face} alt="Facebook" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
